d26b00630cce59977f5813241746c80f662eceec
Eric Chen <echen9870@gmail.com>
2024-09-14 12:15:34 -0400

    Modified personalModel Schema

--- models/personalModel.js
+++ models/personalModel.js
@@ -1,17 +1,24 @@
-const mongoose = require('mongoose');
-const generateHashId = require('../helper/hash'); // Adjust path as necessary
+const mongoose = require("mongoose");
+const generateHashId = require("../helper/hash"); // Adjust path as necessary
 
 const personalSchema = new mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   userName: { type: String, required: true },
   hashID: { type: String, required: true, unique: true },
+  userMRN: { type: String, required: true },
   userPhone: { type: String, required: true },
   userEmail: { type: String, required: true, unique: true },
   userHeight: { type: Number, required: true },
   userWeight: { type: Number, required: true },
+  userInsurance: { type: String, required: true },
   userHashedPassword: { type: String, required: true },
+  userGender: {
+    type: String,
+    enum: ["He/Him", "She/Her", "They/Them", "Other"],
+    default: "Other",
+  },
 });
 
-const PersonalModel = mongoose.model('Personal', personalSchema);
+const PersonalModel = mongoose.model("Personal", personalSchema);
 
 module.exports = PersonalModel;
