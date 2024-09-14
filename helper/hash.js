abfd45b6c9f9a44ffde39c06fedcafa85a1610a2
Ashitha Gowda <ashithapgowdaapg@gmail.com>
2024-09-14 15:47:27 -0400

    added a hash digest for password

--- helper/hash.js
+++ helper/hash.js
@@ -7,4 +7,11 @@ const generateHashId = (userName, userEmail) => {
   return fullHash.substring(0, 7); // Return the first 7 characters
 };
 
+const generateHashPassword = (password) => {
+  const hash = crypto.createHash('sha256');
+  hash.update(userName);
+  const fullHash = hash.digest('hex');
+  return fullHash.substring(0, 7); // Return the first 7 characters
+};
+
 module.exports = generateHashId;
