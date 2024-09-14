b43a0e2d537fc97b0d36721e88279d15550a0d0b
Ashitha Gowda <ashithapgowdaapg@gmail.com>
2024-09-14 15:47:43 -0400

    made changes to user schema

--- models/userModel.js
+++ models/userModel.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   userName: { type: String, required: true },
-  hashID: { type: String, required: true, unique: true },
-  userPhone: { type: String, required: true },
+  cardID: { type: String, required: true, unique: true },
+  userPhone: { type: String, required: false },
   userEmail: { type: String, required: true, unique: true },
-  userHashedPassword: { type: String, required: true },
+  userHashedPassword: { type: String, required: false },
   role : {type : String, required: true},
   ssoSessionToken : {type : String, required: false},
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  tokenExpiry : { type: String, required: false }
 });
 
 const UserModel = mongoose.model('User',userSchema);
