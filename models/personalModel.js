1655274ba6a1ca9c4be2ffd5d5acdabd1e98bed6
Eric Chen <echen9870@gmail.com>
2024-09-14 00:25:59 -0400

    changed personID to hashID

--- models/personalModel.js
+++ models/personalModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const personalSchema = new mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   userName: { type: String, required: true },
-  userID: { type: String, required: true },
+  hashID: { type: String, required: true },
   userEmail: { type: String, required: true },
   userHeight: { type: Number, required: true },
   userWeight: { type: Number, required: true },
