83c04ea48689e019b5fcbb5627e85834205e2696
Ashitha Gowda <ashithapgowdaapg@gmail.com>
2024-09-14 04:11:37 -0400

    added a new user schema

---
+++ models/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+  userName: { type: String, required: true },
+  hashID: { type: String, required: true, unique: true },
+  userPhone: { type: String, required: true },
+  userEmail: { type: String, required: true, unique: true },
+  userHashedPassword: { type: String, required: true },
+  role : {type : String, required: true}
+});
+
+const userModel = mongoose.model('User',userSchema);
+
+module.exports = userModel;
